16111aa1deaea7181e68590d066a828fb8b928de
Magnum Fonseca <magnumfonseca@Magnums-MacBook-Pro.local>
2019-12-26 14:54:02 -0300

    Exercise 3

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+const HomeScreen = ({ navigation }) => {
+  return <View>
+    <Text style={styles.text}>Hi there!</Text>
+    <Button
+      onPress={() => navigation.navigate('Components')}
+      title="Got to Components Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('List')}
+      title="Go To List Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Image')}
+      title="Go To Images"
+    />
+  </View>
 };
 
 const styles = StyleSheet.create({
