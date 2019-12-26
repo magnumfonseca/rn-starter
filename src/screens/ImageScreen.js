ab5d5f6fda8f868f3ead19ac4294bcf5901d96fa
Magnum Fonseca <magnumfonseca@Magnums-MacBook-Pro.local>
2019-12-26 15:16:06 -0300

    Exercise 4

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-    return <Text> Image Screen</Text>
+    return <View>
+        <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score="9" />
+        <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score="7" />
+        <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score="4" />
+    </View>;
 }
 
 const styles = StyleSheet.create({});
